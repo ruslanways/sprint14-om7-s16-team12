f35dda024b99f6109ead77bad70b79c6b821c1fc
Maksym Fil <mfil040503@gmail.com>
2022-04-02 18:25:14 +0200

    Book by id

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('',views.main_page),
     path('book_list/',views.book_list,name="book_list"),
+    path('book_list/id_<int:id>/',views.book_by_id,name="book_page"),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 from book.models import Book
 # Create your views here.
 def main_page(request):
@@ -8,4 +8,7 @@ def book_list(request):
     content=[i for i in Book.objects.all()]
     final=([[i.id,i.name,i.description,[author for author in i.authors.all()]] for i in content])
     return render(request,"Books/book_list.html",{"content":content})
+def book_by_id(request,id):
+    content=Book.objects.get(id=id)
+    return render(request,"Books/book_page.html",{"book":content})
 
